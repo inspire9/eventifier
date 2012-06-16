1fc8726261545d0721add7a02437e2c390ce74b1
PJ Murray <pj.murray.nz@gmail.com>
2012-06-17 03:32:27 +1000

    tidied up eventobserver

--- lib/eventifier/event_observer.rb
+++ lib/eventifier/event_observer.rb
@@ -1,12 +1,17 @@
+require 'active_support/concern'
+
 module Eventifier
+  module EventObserverMethods
+    extend ActiveSupport::Concern
 
-  if defined? ActiveRecord
-    class EventObserver < ActiveRecord::Observer
+    include do
       observe Eventifier::Event
+    end
 
+    module InstanceMethods
       def add_notification klass_name, relation, method
         observed_classes.each do |observed_class|
-          notification_mappings[klass_name.name] ||= { }
+          notification_mappings[klass_name.name]         ||= { }
           notification_mappings[klass_name.name][method] = relation
         end
       end
@@ -34,40 +39,19 @@ module Eventifier
         @notification_mapppings ||= { }
       end
     end
+  end
+
+  if defined? ActiveRecord
+
+    class EventObserver < ActiveRecord::Observer
+      include Eventifier::EventObserverMethods
+
+    end
   elsif defined? Mongoid
 
     class EventObserver < Mongoid::Observer
-      observe Eventifier::Event
+      include Eventifier::EventObserverMethods
 
-      def add_notification klass_name, relation, method
-        observed_classes.each do |observed_class|
-          notification_mappings[klass_name.name] ||= { }
-          notification_mappings[klass_name.name][method] = relation
-        end
-      end
-
-      def after_create event
-        Rails.logger.info "Firing #{event.eventable_type}##{event.verb} - #{notification_mappings[event.eventable_type][event.verb]}" if notification_mappings.has_key?(event.eventable_type) and notification_mappings[event.eventable_type].has_key?(event.verb) and defined?(Rails)
-        method_from_relation(event.eventable, notification_mappings[event.eventable_type][event.verb]).each do |user|
-          next if user == event.user
-          Eventifier::Notification.create :event => event, :user => user
-        end if notification_mappings.has_key?(event.eventable_type) and notification_mappings[event.eventable_type].has_key?(event.verb)
-      end
-
-      def method_from_relation object, relation
-        if relation.kind_of?(Hash)
-          method_from_relation(proc { |object, method| object.send(method) }.call(object, relation.keys.first), relation.values.first)
-        else
-          send_to = proc { |object, method| object.send(method) }.call(object, relation)
-          send_to = send_to.kind_of?(Array) ? send_to : [send_to]
-        end
-      end
-
-      private
-
-      def notification_mappings
-        @notification_mapppings ||= { }
-      end
     end
   end
 end
