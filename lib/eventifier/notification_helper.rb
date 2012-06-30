2f0e19d49feeeeff85c556047d0237fbd5d7b1d8
PJ Murray <pj.murray.nz@gmail.com>
2012-06-30 14:44:09 +1000

    allow specific messages for a particular updated attribute

--- lib/eventifier/notification_helper.rb
+++ lib/eventifier/notification_helper.rb
@@ -13,16 +13,18 @@ module Eventifier
     #     %li= notification_message notification.event
 
     def notification_message event
+      default = "notifications.default.#{event.verb}".to_sym
       if event.verb.to_sym == :update
         if event.change_data.keys.count == 1
-          key = "notifications.#{event.eventable_type.downcase}.#{event.verb}.single"
+          key = "notifications.#{event.eventable_type.downcase}.#{event.verb}.attributes.#{event.change_data.keys.first}"
+          default = ["notifications.#{event.eventable_type.downcase}.#{event.verb}.single".to_sym, default]
         else
           key = "notifications.#{event.eventable_type.downcase}.#{event.verb}.multiple"
         end
       else
         key = "notifications.#{event.eventable_type.downcase}.#{event.verb}"
       end
-      message = I18n.translate key, :default => :"notifications.default.#{event.verb}", "user.name" => event.user.name, :"event.type" => event.eventable_type
+      message = I18n.translate key, :default => default, "user.name" => event.user.name, :"event.type" => event.eventable_type
 
       replace_vars(message, event).html_safe
     end
