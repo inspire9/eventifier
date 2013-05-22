ab52efc9a645d4df156d6d474f78ec61414243ab
Nathan S <nathan@inspire9.com>
2013-05-22 17:59:56 +1000

    use ActiveSupport::Notifications for catching events

--- lib/eventifier/trackable_class.rb
+++ lib/eventifier/trackable_class.rb
@@ -12,27 +12,29 @@ class Eventifier::EventTracking::TrackableClass
   def track
     add_relations
 
-    create_callbacks
+    generate_callbacks
 
     observer.instance
+
+    subscribe_to_events
   end
 
   private
   def add_relations
-    @klass.class_eval { has_many :events, :as => :eventable, :class_name => 'Eventifier::Event', :dependent => :destroy }
+    @klass.class_eval { has_many :events, as: :eventable, class_name: 'Eventifier::Event', dependent: :destroy }
     add_notification_association(@klass)
   end
 
-  # create a callback for the methods we want to track
-  def create_callbacks
+  # generate a callback on the observer for the methods we want to track
+  def generate_callbacks
     methods = @klass_methods
     attributes = @attributes
+    klass = @klass
 
     observer.class_eval do
       methods.each do |event|
         define_method "after_#{event}" do |object|
-          # create an event when the callback is fired
-          Eventifier::Event.create_event(event.to_sym, object, attributes) if object.changed?
+          ActiveSupport::Notifications.instrument("#{event}.#{klass.name.tableize}.eventifier", event: event.to_sym, object: object, options: attributes) if object.changed?
         end
       end
     end
@@ -44,6 +46,22 @@ class Eventifier::EventTracking::TrackableClass
       klass.observe @klass
 
       klass
+    end
+  end
+
+  def subscribe_to_events
+    @klass_methods.each do |method_name|
+      subscribe_to_method "#{method_name}.#{@klass.name.tableize}.eventifier"
+    end
+  end
+
+  def subscribe_to_method name
+    return if ActiveSupport::Notifications.notifier.listening?(name)
+
+    ActiveSupport::Notifications.subscribe name do |*args|
+      event = ActiveSupport::Notifications::Event.new(*args)
+
+      Eventifier::Event.create_event(event.payload[:event].to_sym, event.payload[:object], event.payload[:options])
     end
   end
 end
