088ac00ac815d763b09c80c0be09bc28e68a3398
Pat Allan <pat@freelancing-gods.com>
2013-06-03 18:09:39 +1000

    And thus, no need for the migration modules.

--- lib/generators/eventifier/install/install_generator.rb
+++ lib/generators/eventifier/install/install_generator.rb
@@ -3,9 +3,6 @@ require 'rails/generators/active_record'
 module Eventifier
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      include Rails::Generators::Migration
-      extend ActiveRecord::Generators::Migration
-
       source_root File.expand_path('../templates', __FILE__)
 
       def copy_event_tracking
