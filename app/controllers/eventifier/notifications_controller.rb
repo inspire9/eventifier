381d060c27742e62ee6f7bb62757a720ad7bbfc8
Pat Allan <pat@freelancing-gods.com>
2013-06-04 19:18:37 +1000

    Pagination, recent notifications.
    after parameter provides timestamp as an integer.
    recent parameter (set to anything) limits results to things newer than the user's last_read_at.
    limit parameter can be used to change the page size.

--- app/controllers/eventifier/notifications_controller.rb
+++ app/controllers/eventifier/notifications_controller.rb
@@ -1,11 +1,31 @@
 class Eventifier::NotificationsController < Eventifier::ApplicationController
   def index
-    @notifications = current_user.notifications.limit(5)
+    @notifications = notifications
   end
 
   def touch
     current_user.update_attribute :notifications_last_read_at, Time.zone.now
 
     render :json => {'status' => 'OK'}
+  end
+
+  private
+
+  def notifications
+    scope = current_user.notifications.limit(per_page)
+    scope = scope.where("created_at < ?", after) if params[:after]
+    scope = scope.where(
+      "created_at > ?", current_user.notifications_last_read_at
+    ) if params[:recent]
+
+    scope
+  end
+
+  def after
+    Time.zone.at params[:after].to_i
+  end
+
+  def per_page
+    (params[:limit] || 5).to_i
   end
 end
