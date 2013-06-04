03f7a558ff0c23b320eab8d3f5da61a3d9b76fd7
Pat Allan <pat@freelancing-gods.com>
2013-06-04 18:27:09 +1000

    partial_view is now partial_path.

--- spec/helpers/eventifier/path_helper_spec.rb
+++ spec/helpers/eventifier/path_helper_spec.rb
@@ -3,17 +3,17 @@ require 'spec_helper'
 describe Eventifier::PathHelper do
   let(:notification) { double(Eventifier::Notification, event: double(Eventifier::Event, eventable_type: 'AwesomeObject')) }
 
-  describe "partial_view" do
+  describe "partial_path" do
     it "returns an app view path with context in the path" do
-      helper.partial_view(notification, :donkey).should == 'eventifier/donkey/notification'
+      helper.partial_path(notification, :donkey).should == 'eventifier/donkey/notification'
     end
 
     it "returns an app view if it's defined" do
-      helper.partial_view(notification, :dropdown).should == 'eventifier/dropdown/awesome_object'
+      helper.partial_path(notification, :dropdown).should == 'eventifier/dropdown/awesome_object'
     end
 
     it "returns the default view if not defined" do
-      helper.partial_view(notification).should == 'eventifier/notification'
+      helper.partial_path(notification).should == 'eventifier/notification'
     end
   end
 end
