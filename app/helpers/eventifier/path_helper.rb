e1dc52afe13622d8ceca819e4ae8c373fa63d653
Pat Allan <pat@freelancing-gods.com>
2013-06-04 17:31:09 +1000

    Send through the path, not an array of path elements.

--- app/helpers/eventifier/path_helper.rb
+++ app/helpers/eventifier/path_helper.rb
@@ -1,7 +1,7 @@
 module Eventifier
   module PathHelper
-    def partial_view notification, context = nil
-      if lookup_context.exists?(notification.event.eventable_type.underscore, [:eventifier, context].compact, true)
+    def partial_view(notification, context = nil)
+      if lookup_context.exists?(notification.event.eventable_type.underscore, [:eventifier, context].compact.join('/'), true)
         [:eventifier, context, notification.event.eventable_type.underscore].compact.join("/")
       else
         [:eventifier, context, 'notification'].compact.join("/")
