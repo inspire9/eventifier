608fcf04ba22f8035db47d5de45a3445ef5ef7dc
Pat Allan <pat@freelancing-gods.com>
2013-10-01 22:55:28 +1000

    :if and :unless options for notify, accepting Procs.

--- lib/eventifier/notification_translator.rb
+++ lib/eventifier/notification_translator.rb
@@ -1,23 +1,31 @@
 class Eventifier::NotificationTranslator
-  def initialize(prefix, delivery, *args)
-    @prefix, @delivery = prefix, delivery
+  def initialize(prefix, options, *args)
+    @prefix, @options = prefix, options
     @event  = ActiveSupport::Notifications::Event.new(*args).payload[:event]
   end
 
   def translate
     users_and_relations do |user, relations|
       next if user == event.user
+      next if skip?(user)
 
       Eventifier::Notification.create event: event, user: user,
         relations: relations
 
-      Eventifier::Delivery.deliver_for user if delivery == :immediate
+      Eventifier::Delivery.deliver_for user if options[:email] == :immediate
     end
   end
 
   private
 
-  attr_reader :event, :prefix, :delivery
+  attr_reader :event, :prefix, :options
+
+  def skip?(user)
+    return !options[:if].call(event, user)    if options[:if]
+    return options[:unless].call(event, user) if options[:unless]
+
+    false
+  end
 
   def users_and_relations(&block)
     Eventifier::NotificationMapping.users_and_relations event, prefix, &block
--- lib/eventifier/notifier/notifier.rb
+++ lib/eventifier/notifier/notifier.rb
@@ -1,4 +1,5 @@
 class Eventifier::Notifier
+  OPTION_KEYS = [:email, :if, :unless]
   # arguments will either be [:relation, {:on => :create}] or
   # [{:relation => :second_relation, :on => :create}]
   # If it's the first one, relation is the first in the array, otherwise treat
@@ -8,14 +9,13 @@ class Eventifier::Notifier
     arguments  = arguments.first || {}
 
     methods    = Array arguments.delete(:on)
-    delivery   = arguments.delete(:email) || :delayed
-
-    relation ||= arguments
+    options    = arguments.slice *OPTION_KEYS
+    relation ||= arguments.except *OPTION_KEYS
 
     klasses.each do |target_klass|
       methods.each do |method_name|
         Eventifier::NotificationMapping.add "#{method_name}.#{target_klass.name.tableize}", relation
-        Eventifier::NotificationSubscriber.subscribe_to_method target_klass, method_name, delivery
+        Eventifier::NotificationSubscriber.subscribe_to_method target_klass, method_name, options
       end
     end
   end
